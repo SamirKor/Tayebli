5cf53bd2ca478cdce48ac920ff05a45d509e2798
SamirKor <samir-abdelhakim.korbas@ensia.edu.dz>
2026-02-06 00:01:22 +0100

    Restore MySQLi connection and configuration settings

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@ $username = "root";      // Default for XAMPP
 $password = "WSmFRcsyVWXWvWJDjwqGnkZBSHNnFuXQ";          // Empty for XAMPP
 $database = "railway";
 // Create MySQLi connection
-//$connection = mysqli_connect($hostname, $username, $password, $database);
+$connection = mysqli_connect($hostname, $username, $password, $database);
 
 // Check connection
 if (!$connection) {
@@ -24,3 +24,4 @@ define('AI_PROVIDER', 'gemini'); // Options: 'gemini' or 'deepseek'
 
 
 ?>
+
